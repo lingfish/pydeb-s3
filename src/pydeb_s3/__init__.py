d87cfc4b93ce49345029624c4176aa0549899571
lingfish <jason@lucid.net.au>
2026-04-24 13:18:56 +1000

    Add hatch-vcs dynamic versioning

--- src/pydeb_s3/__init__.py
+++ src/pydeb_s3/__init__.py
@@ -1,3 +1,6 @@
 """pydeb-s3 - A simple utility to make creating and managing APT repositories on S3."""
 
-__version__ = "0.1.0"
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.1.0"
